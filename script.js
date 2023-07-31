a0c21d540bdb248a0e9b22cf92127a2c6a994c84
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-07-31 09:57:10 +0530

    done4

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const addTaskpopup = document.getElementById("popbox");
-const main = document.getElementById("maincontainer")
+// const main = document.getElementById("maincontainer")
 const cardContainer =  document.getElementById("card-container");
 const newCardname = document.getElementById("card-name");
-console.log(newCardname.parentNode);
-let noitem = document.getElementById("no-item");
-let cardID = 0;
-let start = 0;
+// console.log(newCardname.parentNode);
+// let noitem = document.getElementById("no-item");
+// let cardID = 0;
+// let start = 0;
 
 
 function showtask(){
-    cardID++;
-    main.setAttribute("class","main");
-    const addTaskpopup = document.getElementById("popbox");
-    addTaskpopup.setAttribute("class","pop-up-box show");
+    // cardID++;
+    // main.setAttribute("class","main");
+    // const addTaskpopup = document.getElementById("popbox");
+    // addTaskpopup.setAttribute("class","pop-up-box show");
     addTaskpopup.classList.remove("hide-box");
-    if(cardID >= 1){
-        noitem.remove();
-    }
+    // if(cardID >= 1){
+    //     noitem.remove();
+    // }
     // popupTaskVisible = true 
 
     // var mainContainer = document.createElement("div")
@@ -32,7 +32,7 @@ function closepopupbox(){
 
 function addCard(){
 
-    cardID++;
+//     cardID++;
     var newCard = document.createElement("div")
     var cardTitle = document.createElement("h2")
     var hrline = document.createElement("hr")
@@ -42,7 +42,7 @@ function addCard(){
     
 
     
-    newCard.setAttribute("id", cardID)
+//     newCard.setAttribute("id", cardID)
     newCard.classList.add("cards")
     hrline.classList.add("hrlines")
     additem.classList.add("addlist")
@@ -55,22 +55,40 @@ function addCard(){
     newCard.appendChild(deletebutton)
     newCard.appendChild(additem)
     cardContainer.appendChild(newCard)
+    
 
     cardTitle.innerText = newCardname.value
     deletebutton.innerText = " "
     additem.innerText = " "
 
-    addTaskpopup.classList.add("hide-box")
-    main.classList.remove("main")
+    // addTaskpopup.classList.add("hide-box")
+    // main.classList.remove("main")
     
-    additem.addEventListener("click",() => {
-        cardnew.style.height = "auto"
-        createItempopup("itemlist");
-    })
+    // additem.addEventListener("click",() => {
+    //     cardnew.style.height = "auto"
+    //     createItempopup("itemlist");
+    // })
 
-    deletebutton.addEventListener("click",function(){
-        newCard.remove()
-    });
+    // deletebutton.addEventListener("click",function(){
+    //     newCard.remove()
+    // });
+
+    // function itempopup(itemlist){
+
+    //     var itemtitle = document.createElement("h3");
+    //     let body = document.querySelector("body");
+    //     let itemsadd = document.createElement("div");
+    //     let itemtitle2 = document.createElement("h2");
+    //     let itemname = document.createElement("input");
+    //     let itemaddbutton = document.createElement("button");
+    //     let itemcloasebutton = document.createElement("button");
+
+
+    //     itemsadd.classList.add("itemsadd")
+    //     itemsadd.style.display = "block";
+    //     itemname
+    //     itemlist.appendChild(itemtitle);
+    // }
 
     closepopupbox()
 
