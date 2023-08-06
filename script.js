00be8f97a37a2393b56bb15623116f948ec58815
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-08-06 21:39:55 +0530

    done

--- script.js
+++ script.js
@@ -1,144 +1,174 @@
+const addTaskpop = document.getElementById("pop-up");
+const main = document.getElementById("MainContainer");
+const noitem = document.getElementById("noitem");
+const pop2 = document.getElementById("dropup2");
+const itembox = document.getElementById("box");
+const center = document.getElementById("center");
+const header = document.querySelector("header");
+const header2 = document.getElementsByClassName("task-List");
+let count = 0;
+let start = 0;
 
-
-const addTaskpopup = document.getElementById("popbox");
-// const main = document.getElementById("maincontainer")
-const cardContainer =  document.getElementById("card-container");
-const newCardname = document.getElementById("card-name");
-console.log(newCardname.parentNode);
-
-// let noitem = document.getElementById("no-item");
-let cardID = 0;
-// let start = 0;
-let additempopup = document.querySelector(".popup-box2");
-console.log(additempopup)
-
-
-function showtask(){
-    // cardID++;
-    // main.setAttribute("class","main");
-    // const addTaskpopup = document.getElementById("popbox");
-    // addTaskpopup.setAttribute("class","pop-up-box show");
-    addTaskpopup.classList.remove("hide-box");
-    // if(cardID >= 1){
-    //     noitem.remove();
-    // }
-    // popupTaskVisible = true 
-
-    // var mainContainer = document.createElement("div")
-    // mainContainer.classList.add("Main-container")
+function showtask() {
+  count++;
+  main.setAttribute("class", "first");
+  let addTaskpop = document.getElementById("pop-up");
+  addTaskpop.setAttribute("class", " drop popupbox");
+  addTaskpop.classList.remove("hide");
+  itembox.classList.add("first");
+  if (count >= 1) {
+    noitem.style.display = "none";
+  }
 }
 
+const newadd = document.getElementById("Add");
+const newcard = document.getElementById("cardnew");
+const newcard2 = document.getElementById("cardnew2");
+const back1 = document.getElementsByClassName("backbutton");
+const tittle5 = document.getElementById("tasklist");
+const tittle6 = document.getElementsByClassName("button1");
+const blank = document.getElementsByClassName("blank");
+const button4 = document.getElementsByClassName("button4");
 
-function closepopupbox(){
-    addTaskpopup.classList.add("hide-box")
-    // popupTaskVisible = false
-    // mainContainer.classList.add("blurpage")
-}
+function cardAdd() {
 
-function addCard(){
-
-    cardID++;
-    var newCard = document.createElement("div")
-    var cardTitle = document.createElement("h2")
-    var hrline = document.createElement("hr")
-    var itemlist = document.createElement("div")
-    var additem = document.createElement("button")
-    var deletebutton = document.createElement("button")
-    
-
-    
-    newCard.setAttribute("id", cardID)
-    newCard.classList.add("cards")
-    hrline.classList.add("hrlines")
-    additem.classList.add("addlist")
-    deletebutton.classList.add("delcard")
+  count++;
+  itembox.classList.remove("first");
+  
+  var cardnew1 = document.createElement("div");
+  var cardtitle = document.createElement("h2");
+  var hrline = document.createElement("hr");
+  var itemlist = document.createElement("div");
+  var deletebutton = document.createElement("button");
+  var addnewbtn = document.createElement("button");
 
 
-    newCard.appendChild(cardTitle)
-    newCard.appendChild(hrline)
-    newCard.appendChild(itemlist)
-    newCard.appendChild(deletebutton)
-    newCard.appendChild(additem)
-    cardContainer.appendChild(newCard)
-    
+  cardtitle.style.marginLeft = "30%";
+  
+  deletebutton.classList.add("btn3");
+  addnewbtn.classList.add("btn4");
+  cardnew1.classList.add("conatiner_child");
+  hrline.classList.add("hrlines")
 
-    cardTitle.innerText = newCardname.value
-    deletebutton.innerText = " "
-    additem.innerText = " "
+  console.log(start);
+  cardnew1.setAttribute("id", start);
+  cardnew1.appendChild(cardtitle);
+  cardnew1.appendChild(hrline);
+  cardnew1.appendChild(itemlist);
+  cardnew1.appendChild(deletebutton);
+  cardnew1.appendChild(addnewbtn);
+  itembox.appendChild(cardnew1);
 
-    // addTaskpopup.classList.add("hide-box")
-    // main.classList.remove("main")
-    
-    // additem.addEventListener("click",() => {
-    //     cardnew.style.height = "auto"
-    //     createItempopup("itemlist");
-    // })
+ 
 
-    deletebutton.addEventListener("click",function(){
-        newCard.remove()
+  cardtitle.innerText = newcard.value;
+  deletebutton.classList.add("delete");
+  addnewbtn.classList.add("add1");
+  addTaskpop.classList.add("hide");
+  main.classList.remove("first");
+
+  addnewbtn.addEventListener("click", () => {
+    cardnew1.style.height = "auto";
+    createItempopup(itemlist);
+  });
+  deletebutton.addEventListener("click", () => {
+    cardnew1.remove();
+  });
+
+  function createItempopup(itemlist) {
+
+    var tittle2 = document.createElement("h3");
+    let body = document.querySelector("body");
+    let popupbox2 = document.createElement("div");
+    let tittle3 = document.createElement("h2");
+    let input3 = document.createElement("input");
+    let Add2 = document.createElement("button");
+    let close2 = document.createElement("button");
+
+
+    popupbox2.classList.add("popupbox2");
+    Add2.classList.add("Add2")
+    close2.classList.add("close2");
+    popupbox2.style.display = "block";
+
+
+    input3.setAttribute("id", "cardnew2");
+    tittle3.innerText = "Add the  List";
+    tittle2.innerText = input3.value;
+
+
+    Add2.innerText = "Add";
+
+    close2.innerText = "close";
+
+    itemlist.appendChild(tittle2);
+    body.appendChild(popupbox2);
+    popupbox2.appendChild(tittle3);
+    popupbox2.appendChild(input3);
+
+
+    popupbox2.appendChild(Add2);
+    popupbox2.appendChild(close2);
+
+    Add2.addEventListener("click", () => {
+      const p = document.createElement("p");
+      p.classList.add("inline");
+      tittle2.innerText = input3.value;
+      popupbox2.style.display = "none";
+      var markdone = document.createElement("button");
+      itemlist.appendChild(p);
+      itemlist.classList.add("space");
+      markdone.innerText = "Done";
+      p.appendChild(tittle2);
+      p.appendChild(markdone);
+      markdone.classList.add("mark");
+      itembox.classList.remove("first");
+      markdone.addEventListener("click", () => {
+        Done();
+      });
+      function Done() {
+        tittle2.style.textDecoration = "line-through";
+        tittle2.style.color = "red";
+        tittle2.style.fontWeight = "bolder";
+        markdone.remove();
+      }
     });
+    close2.addEventListener("click", () => {
+      popupbox2.remove();
+      console.log(itembox.childNodes);
 
-    closepopupbox()
+    });
+  }
+  cardtitle.addEventListener("click", () => {
+    itembox.style.display = "none";
+    center.style.display = "block";
+    center.appendChild(cardnew1);
+    center.style.display = "flex";
+    header.style.display = "block";
+    tittle5.style.display = "none";
+    tittle6[0].style.display = "none";
+    blank[0].innerText = cardtitle.innerText;
+  });
 
-    
-    let newitemadd = document.querySelector("addlist");
-    let Itemid = 0;
-    additem.addEventListener("click",function additem(){
+  back1[0].addEventListener("click", () => {
+    backbutton();
+  });
 
-        additempopup.classList.remove("hide-box");
-        addTaskpopup.classList.add("show");
+  function backbutton() {
+    console.log("Hii");
+    center.style.display = "none";
+    center.innerText = " ";
+    itembox.appendChild(cardnew1);
+    itembox.style.display = "flex";
+    itembox.style.flexDirection = "row";
+    itembox.style.justifyContent = "space-between";
+    header.style.display = "none";
+    tittle5.style.display = "block";
+    tittle6[0].style.display = "block";
+  }
+}
 
-        function closepopupbox2(){
-            additem.classList.remove("hide-box");
-        }
-        closepopupbox2()
-        
-        // Itemid++;
-        // var newitem = document.createElement("div");
-        // var boxname = document.createElement("h2");
-        // var itemname = document.createElement("input");
-        // var add = document.createElement("button");
-        // var del = Document.createElement("button");
-
-        // newitem.appendChild(h2)
-        // newitem.appendChild(boxname)
-        // newitem.appendChild(itemname)
-
-        // newitem.classList.add("newitembox");
-        // boxname.classList.add("boxname");
-        // itemname.classList.add("itemlistname");
-    })
-
-
-    // const additempopup = document.getElementById("popbox2");
-
-    // function showPopup2(){
-    //     additempopup.classList.remove("hide-box");
-    // }
-
-    // function closepopupbox2(){
-    //     additempopup.classList.add("hide-box")
-    // }
-
-
-
-    // function itempopup(itemlist){
-
-    //     var itemtitle = document.createElement("h3");
-    //     let body = document.querySelector("body");
-    //     let itemsadd = document.createElement("div");
-    //     let itemtitle2 = document.createElement("h2");
-    //     let itemname = document.createElement("input");
-    //     let itemaddbutton = document.createElement("button");
-    //     let itemcloasebutton = document.createElement("button");
-
-
-    //     itemsadd.classList.add("itemsadd")
-    //     itemsadd.style.display = "block";
-    //     itemname
-    //     itemlist.appendChild(itemtitle);
-    // }
-
-    // closepopupbox()
-
+function closetask() {
+  addTaskpop.setAttribute("class", "dropdown drop hide");
+  main.classList.remove("first");
 }
